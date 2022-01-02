829434fdc8e54875e0b6c58fadaa1f8eaabe6ded
Mostafa AbdElHamid <mhsmustafa84@gmail.com>
2022-01-02 15:19:22 +0200

    complete the project

---
+++ src/components/error.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Box, } from '@material-ui/core';
+
+export const Error = () => {
+    return (
+        <Box color={"darkred"}>
+            <h2>Please enter a correct City and Country!!</h2>
+        </Box>
+    )
+}
