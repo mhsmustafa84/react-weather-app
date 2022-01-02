5fd31140e3863d6e585743bec48113b287c74f8a
Mostafa AbdElHamid <mhsmustafa84@gmail.com>
2022-01-02 15:52:56 +0200

    fix style

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -6,12 +6,13 @@ export const Weather = ({ result }) => {
         const { name, sys, main, weather } = result.data;
         return (
             <Box bgcolor={"rgb(255, 193, 58)"} py={3}>
-                <h2>City: {name}, Country: {sys.country}</h2>
-                <img src={`https://openweathermap.org/img/wn/${weather[0].icon}.png`} alt="icon" />
-                <Box mx={2} fontSize={40}>
+                <h2>City: {name}</h2>
+                <h3>Country: {sys.country}</h3>
+                <Box mx={2} fontSize={30}>
                     Temp: {Math.floor(main.temp - 273.25)}&deg;
                 </Box>
                 <p>Status: {weather[0].description}</p>
+                <img src={`https://openweathermap.org/img/wn/${weather[0].icon}.png`} alt="icon" />
             </Box>
         )
     } else {
